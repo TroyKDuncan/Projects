72aff01c0d566abd9a3526c5604a0e7a1c219cbf
TroyKDuncan <troyduncanbusiness@gmail.com>
2024-05-22 12:06:40 -0500

    Finished sending emails with Python

--- Emails/email_sender.py
+++ Emails/email_sender.py
@@ -1,3 +1,21 @@
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
 
+
+# Template object can replace things with $ in front of them
+html = Template(Path('index.html').read_text())
+email = EmailMessage()
+email['from'] = 'TroyBoi'
+email['to'] = 'tkduncan614@gmail.com'
+email['subject'] = 'You won $1,000,000!!!'
+
+email.set_content(html.substitute({'name': 'WinWin'}), 'html')
+
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+    smtp.ehlo()  # say hello to server
+    smtp.starttls()  # encryption
+    smtp.login('itmehi18@gmail.com', 'pvou gbsf kdhu yyjr')
+    smtp.send_message(email)
+    print('Job\'s done!')
