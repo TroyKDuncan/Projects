4731bcdb4da348e9727ccc46bc527ae74bbd930f
TroyKDuncan <troyduncanbusiness@gmail.com>
2024-05-25 16:30:14 -0500

    Finished web scraper

--- Python/Web_Scraping/webscraper.py
+++ Python/Web_Scraping/webscraper.py
@@ -1,10 +1,45 @@
-import requests  # like a web browser without the GUI
+import requests
 from bs4 import BeautifulSoup
+import pprint
 
-res = requests.get('https://news.ycombinator.com/')
+res = requests.get('https://news.ycombinator.com/news')
+res2 = requests.get('https://news.ycombinator.com/news?p=2')
 soup = BeautifulSoup(res.text, 'html.parser')
+soup2 = BeautifulSoup(res2.text, 'html.parser')
 
-print(soup.body)  # Read the docs for all the different things you can do
-print(soup.body.contents)
-print(soup.find_all('a'))
-print(soup.find('a'))
+# print(soup.body)  # Read the docs for all the different things you can do
+# print(soup.body.contents)
+# print(soup.find_all('a'))
+# print(soup.find('a'))
+
+# Selectors
+# print(soup.select('.score'))  # selects everything with "score" class using the .
+# print(soup.select('#score_40449299'))  # selects item with id="score_40449299" class using the #
+
+links = soup.select('.titleline > a')
+subtext = soup.select('.subtext')
+links2 = soup2.select('.titleline > a')
+subtext2 = soup2.select('.subtext')
+
+combined_links = links + links2
+combined_subtext = subtext + subtext2
+
+
+def sort_stories_by_votes(hnlist):
+    return sorted(hnlist, key=lambda k: k['votes'], reverse=True)
+
+
+def create_custom_hn(links, subtext):
+    hn = []
+    for idx, item in enumerate(links):
+        title = item.getText()
+        href = item.get('href', None)
+        vote = subtext[idx].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            if points > 99:
+                hn.append({'title': title, 'link': href, 'votes': points})
+    return sort_stories_by_votes(hn)
+
+
+pprint.pprint(create_custom_hn(combined_links, combined_subtext))
