6412bf76550ed8251b12dd4bd5f73e825e49fc9e
Troy <troyduncanbusiness@gmail.com>
2024-05-30 00:30:38 -0500

    Still working on web development with Flask

--- Python/Web_Dev/web_server/app.py
+++ Python/Web_Dev/web_server/app.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return render_template('index.html')
+@app.route("/<username>/<int:my_num>")
+def hello_world(username=None, my_num=None):
+    return render_template('index.html', name=username, on_page_num=my_num)
 
 
 @app.route("/about")
