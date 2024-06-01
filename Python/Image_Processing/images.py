f9ec342e0b2bea3dfdef3a747c5f44c4baf997bc
Troy <troyduncanbusiness@gmail.com>
2024-05-31 22:17:36 -0500

    Finished learning Flask

--- Python/Image_Processing/images.py
+++ Python/Image_Processing/images.py
@@ -1,35 +1,35 @@
 from PIL import Image, ImageFilter
 
-img = Image.open('./Pokedex/pikachu.jpg')  # this is now an image object
+img = Image.open('./background.jpg')  # this is now an image object
 
-print(img)
-print(img.format)
-print(img.size)
-print(img.mode)
-print()
+# print(img)
+# print(img.format)
+# print(img.size)
+# print(img.mode)
+# print()
 # print(dir(img))
 
 # apply a filter
-filtered_img = img.filter(ImageFilter.BLUR)
+# filtered_img = img.filter(ImageFilter.BLUR)
 # must be PNG because jpg isn't supported with the ImageFilter
-filtered_img.save('blur.png', 'png')
+# filtered_img.save('blur.png', 'png')
 
 # convert it to greyscale and save it
-filtered_img = img.convert('L')
-filtered_img.save('grey.png', 'png')
+# filtered_img = img.convert('L')
+# filtered_img.save('grey.png', 'png')
 
 # rotate, resize, show
-crooked = filtered_img.rotate(90)
-resize = filtered_img.resize((300, 300))  # Takes a tuple
+# crooked = filtered_img.rotate(90)
+# resize = filtered_img.resize((300, 300))  # Takes a tuple
 # resize.show()
 
 # crop
-cropped_img = img.crop((100, 100, 400, 400))
-cropped_img.save('cropped.png', 'png')
+cropped_img = img.crop((0,500,2500,2000))
+cropped_img.save('cropped5.jpg', 'JPEG')
 # cropped_img.show()
 
 # new image scaling and thumbnail
-img = Image.open('./beach.jpg')
-img.thumbnail((400,400))
-img.show()
+# img = Image.open('./beach.jpg')
+# img.thumbnail((400,400))
+# img.show()
 
--- Python/Web_Dev/web_server/app.py
+++ Python/Web_Dev/web_server/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import csv
 
 app = Flask(__name__)
 
@@ -13,6 +14,32 @@ def html_page(page_name):
     return render_template(page_name)
 
 
+def write_to_csv(data):
+    with open('database.csv', newline='', mode='a') as db:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(
+            db, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+
+def write_to_txt(data):
+    with open('database.txt', mode='a') as db:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        db.write(f'\n{email},{subject},{message}')
+
+
 @app.route("/submit_form", methods=['POST', 'GET'])
-def submit_form(page_name):
-    return "Form was submitted"
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+        except:
+            return 'Did not save to database'
+    else:
+        return 'Something went wrong, try again'
