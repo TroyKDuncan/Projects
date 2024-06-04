8dac91cd854c435722034f5764e803fe373f75d2
TroyKDuncan <troyduncanbusiness@gmail.com>
2024-06-04 16:25:03 -0500

    Started machine learning module

---
+++ Python/Machine_Learning/101.py
@@ -0,0 +1,14 @@
+#1 - import the data
+#2 - clean the data
+#3 - split data into training and test sets
+#4 - Create a Model
+#5 - check the output
+#6 - improve
+
+# tools
+
+# NumPy
+# Pandas
+# scikit learn
+# matplotlib
+# jupyter
